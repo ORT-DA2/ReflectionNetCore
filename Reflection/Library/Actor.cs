e0b43ccc84d2a242cd7e1b62947520cfca78d0ac
Ramiro Visca <ramiroviszan@gmail.com>
2018-10-12 08:04:50 -0300

    Example

---
+++ Reflection/Library/Actor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Interface;
+
+namespace Library
+{
+    public class Actor : IPerson
+    {
+        public string firstname;
+        private string surname;
+
+        public string Name { get { return  firstname + surname; } }
+
+        public Actor(string aName, string aSurname) {
+            firstname = aName;
+            surname = aSurname;
+        }
+
+        public string SayHello(string to) {
+            return "Hellooo! " + to;
+        }
+    }
+}
